8528fb52c6d212014ead015773b45cebf39baeeb
Christofer Berruz <cberruzc@my.bridgeport.edu>
2020-08-23 20:46:54 -0400

    Modifications

---
+++ component---src-pages-404-js-58d48671ae49184cc577.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return l}));var n=t("q1tI"),c=t.n(n),r=t("Wbzz");function l(){return c.a.createElement("div",{className:"container"},c.a.createElement("div",{className:"row"},c.a.createElement("div",{className:"col-12"},c.a.createElement("h2",null," Error 404"),c.a.createElement("p",null," Let me take you back chief... "),c.a.createElement(r.a,{to:"/home"}," Go back to main page"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-58d48671ae49184cc577.js.map
