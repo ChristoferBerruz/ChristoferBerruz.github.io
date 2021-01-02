9be60bca1936c8838dec7515962e1380d65be1da
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-01-01 20:13:41 -0500

    Updates

---
+++ component---src-pages-blog-post-1-js-be1928c9c0acae261411.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"9NvE":function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return u}));var o=e("q1tI"),r=e.n(o);function u(){return r.a.createElement("div",null,"Hello from blog post")}}}]);
+//# sourceMappingURL=component---src-pages-blog-post-1-js-be1928c9c0acae261411.js.map
